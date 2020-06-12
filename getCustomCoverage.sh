76a067f4e94768affc42c87e20221d79d1d62bad
erikwaskiewicz <32075672+erikwaskiewicz@users.noreply.github.com>
2020-06-12 15:05:07 +0100

    Change to v1.0.0

--- getCustomCoverage.sh
+++ getCustomCoverage.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 # USE: bash inside run folder - will iterate over all samples. Depends on R script
 # calculateTargetCoverage.R (located in same folder).
 
-version="2.6.0"
+version="1.0.0"
 
 RUN_DIR=$PWD
 
